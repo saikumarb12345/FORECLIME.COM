a1d1d33e664002fbcdbe91f6e799375aa60898c5
SaiKumar <bsainaidu90@gmail.com>
2023-10-02 12:27:51 +0530

    Fixed issue with weather sharing to reflect dynamic city selection

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// JavaScript to fetch weather data from OpenWeather API
 document.addEventListener("DOMContentLoaded", function () {
   const apiKey = "9b47e2ec44dee886bb405198b7a4b6b3"; // OpenWeather API key
   const form = document.querySelector("#weather-form");
@@ -10,7 +9,39 @@ document.addEventListener("DOMContentLoaded", function () {
   const cityNameElement = document.querySelector("#city-name");
   const shareButton = document.querySelector("#share-button");
 
-  // Code for fetching weather data from api
+  // Function to update the sharing data based on the selected city
+  function updateSharingData(selectedLocation) {
+    const title = `Check out the weather in ${selectedLocation.toUpperCase()}!`;
+    const weatherText = `Current weather in ${selectedLocation.toUpperCase()}: [From ForeClime(https://foreclime.vercel.app/)]\n
+    - Weather Condition: ${weatherConditionValue.textContent.toUpperCase()}
+    - Temperature: ${temperatureValue.textContent}
+    - Humidity: ${humidityValue.textContent}
+    - Wind Speed: ${windSpeedValue.textContent}`;
+    return {
+      title,
+      text: weatherText,
+    };
+  }
+
+  // Function to handle sharing when the share button is clicked
+  async function shareWeatherText() {
+    try {
+      const selectedLocation = locationSelect.value;
+      const sharingData = updateSharingData(selectedLocation);
+
+      // Web Share API to provide sharing options for text
+      if (navigator.share) {
+        await navigator.share(sharingData);
+      } else {
+        // Fallback for browsers that don't support Web Share API
+        alert("Sharing not supported on this browser.");
+      }
+    } catch (error) {
+      console.error("Error sharing weather:", error);
+    }
+  }
+
+  // Code for fetching weather data from the API
   form.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
 
@@ -22,60 +53,18 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((data) => {
         // Update weather information in the HTML
         temperatureValue.textContent = `${data.main.temp}°C`;
-        weatherConditionValue.textContent =
-          data.weather[0].description.toUpperCase();
+        weatherConditionValue.textContent = data.weather[0].description.toUpperCase();
         humidityValue.textContent = `${data.main.humidity}%`;
         windSpeedValue.textContent = `${data.wind.speed} km/h`;
 
         // Updating the city name dynamically
         cityNameElement.textContent = selectedLocation.toUpperCase();
 
-        // ForeClime Website Url
-        function websiteUrl() {
-          const webUrl = "https://foreclime.vercel.app/";
-          return webUrl;
-        }
-
-        // Function to generate the URL [REMOVED]
-
-        // Function to generate the shareable text and URL
-        function generateShareableText() {
-          const title = `Check out the weather in ${selectedLocation.toUpperCase()}! From ForeClime(${websiteUrl()})`;
-          const weatherText = `Current weather in ${selectedLocation.toUpperCase()}:\n
-          - Weather Condition: ${data.weather[0].description.toUpperCase()}
-          - Temperature: ${data.main.temp}°C
-          - Humidity: ${data.main.humidity}%
-          - Wind Speed: ${data.wind.speed} km/h`;
-        //   [REMOVED]const shareURL = generateShareableURL(); // Calling the existing function to generate the URL
-          return `${title}\n\n${weatherText}`;
-        }
-
-        // Function to handle sharing when the share button is clicked
-        async function shareWeatherText() {
-          try {
-            const shareText = generateShareableText();
-
-            // Web Share API to provide sharing options for text
-            if (navigator.share) {
-              await navigator.share({ text: shareText });
-            } else {
-              // Fallback for browsers that don't support Web Share API
-              alert("Sharing not supported on this browser.");
-            }
-          } catch (error) {
-            console.error("Error sharing weather:", error);
-          }
-        }
-
-        // Attached the shareWeatherText function to the share button
-        const shareButton = document.getElementById("share-button");
+        // Attached the shareWeatherText function to the share button click event
         shareButton.addEventListener("click", shareWeatherText);
       })
       .catch((error) => {
-        console.error(
-          "Error fetching weather data try 30 seconds later:",
-          error
-        );
+        console.error("Error fetching weather data try 30 seconds later:", error);
       });
   });
 });
